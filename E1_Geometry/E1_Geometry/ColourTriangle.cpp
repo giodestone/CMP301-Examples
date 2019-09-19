1a6c6f559f75b03fe753dd3c98fb3ffdcbf622aa
giodestone <37457035+giodestone@users.noreply.github.com>
2019-09-19 11:10:47 +0100

    Updated LFS trackd files and  tracked them.

--- E1_Geometry/E1_Geometry/ColourTriangle.cpp
+++ E1_Geometry/E1_Geometry/ColourTriangle.cpp
@@ -23,27 +23,34 @@ void ColourTriangle::initBuffers(ID3D11Device* device)
 {
 	D3D11_SUBRESOURCE_DATA vertexData, indexData;
 
-	vertexCount = 3;
-	indexCount = 3;
+	vertexCount = 4;
+	indexCount = 6;
 
 	VertexType_Colour* vertices = new VertexType_Colour[vertexCount];
 	unsigned long* indices = new unsigned long[indexCount];
 
 	// Load the vertex array with data.
-	vertices[0].position = XMFLOAT3(0.0f, 1.0f, 0.0f);  // Top.
+	vertices[0].position = XMFLOAT3(-1.0f, 1.0f, 0.0f);  // Top left.
 	vertices[0].colour = XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
 
-	vertices[1].position = XMFLOAT3(-1.0f, 0.0f, 0.0f);  // bottom left.
+	vertices[1].position = XMFLOAT3(-1.0f, -1.0f, 0.0f);  // bottom left.
 	vertices[1].colour = XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
 
-	vertices[2].position = XMFLOAT3(1.0f, 0.0f, 0.0f);  // bottom right.
+	vertices[2].position = XMFLOAT3(1.0f, -1.0f, 0.0f);  // bottom right.
 	vertices[2].colour = XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
 
+	vertices[3].position = XMFLOAT3(1.0f, 1.0f, 0.0f);  // Top right.
+	vertices[3].colour = XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
 
-	// Load the index array with data.
-	indices[0] = 0;  // Top/
+
+	// Load the index array with data. Look at where the data in vertecies is.
+	// REMEMBER COUTERCLOCKWISE! AND ONLY TRIANGLES!
+	indices[0] = 0;  // Top left
 	indices[1] = 1;  // Bottom left.
 	indices[2] = 2;  // Bottom right.
+	indices[3] = 0; // Top left
+	indices[4] = 2; // Top bottom right
+	indices[5] = 3; // Top right
 
 	D3D11_BUFFER_DESC vertexBufferDesc = { sizeof(VertexType_Colour) * vertexCount, D3D11_USAGE_DEFAULT, D3D11_BIND_VERTEX_BUFFER, 0, 0, 0 };
 	vertexData = { vertices, 0 , 0 };
