af4d53791ddfc129a8fedde672c6d5f317f9f48b
feliks <email@email.com>
2019-11-11 17:37:52 +0000

    E7 Added more blur.

--- E7_Blur/E7_Blur/shaders/verticalBlur_ps.hlsl
+++ E7_Blur/E7_Blur/shaders/verticalBlur_ps.hlsl
@@ -15,9 +15,10 @@ struct InputType
 
 float4 main(InputType input) : SV_TARGET
 {
-    float weight0, weight1, weight2;
     float4 colour;
 
+#ifdef TWO_PIXEL_BLUR
+    float weight0, weight1, weight2;
 	// Create the weights that each neighbor pixel will contribute to the blur.
 	weight0 = 0.4062f;
     weight1 = 0.2442f;
@@ -36,7 +37,27 @@ float4 main(InputType input) : SV_TARGET
  
     // Set the alpha channel to one.
     colour.a = 1.0f;
+#else
+	float weight0 = 0.382928, weight1 = 0.241732, weight2 = 0.060598, weight3 = 0.005977, weight4 = 0.000229;
+	
+	// Initialize the colour to black.
+	colour = float4(0.0f, 0.0f, 0.0f, 0.0f);
 
+	float texelSize = 1.0f / screenHeight;
+
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * -4.0f)) * weight4;
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * -3.0f)) * weight3;
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * -2.0f)) * weight2;
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * -1.0f)) * weight1;
+	colour += shaderTexture.Sample(SampleType, input.tex) * weight0;
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * 1.0f)) * weight1;
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * 2.0f)) * weight2;
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * 3.0f)) * weight3;
+	colour += shaderTexture.Sample(SampleType, input.tex + float2(0.0f, texelSize * 4.0f)) * weight4;
+
+	// Set the alpha channel to one.
+	colour.a = 1.0f;
+#endif
     return colour;
 }
 
